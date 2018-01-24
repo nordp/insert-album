469855acf03c8161144be1a3117a8a12362826ff
Philip <nordp@student.chalmers.se>
2018-01-24 22:43:13 +0100

    -

--- Frontend/index.js
+++
@@ -1,9 +0,0 @@
-var request = require('browser-request');
-		
-document.addEventListener("DOMContentLoaded", function(event) {
-    request('http://insert-album.appspot.com', function(er, response, body) {
-        if(er)
-            throw er;
-        console.log("I got: " + body);
-    });
-});
