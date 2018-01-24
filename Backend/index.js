f9eef4e48fdc6a25fd295331ab5ebacd72d5e740
Philip <nordp@student.chalmers.se>
2018-01-24 22:39:49 +0100

    added cors

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+var cors = require('cors');
 const app = express();
+
+app.use(cors());
+app.options('*', cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
